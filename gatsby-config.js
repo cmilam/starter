9c3556129fd9843d19f29287e9e911d196823346
craigmilam <craig.milam@nearform.com>
2021-01-17 11:54:55 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  siteMetadata: {
+    title: "headphones",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "JcqBSD1b8g-o-Rvaovp3hKkuJr82PZX1N6DQYNpwbmM",
+        spaceId: "evegefjcr57r",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
