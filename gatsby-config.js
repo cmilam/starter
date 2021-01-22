8562236458a9e80da78ca4fb857d14b72e8ddcaf
cmilam <craig.milam@nearform.com>
2021-01-22 15:47:34 +0100

    revert gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,14 @@
 module.exports = {
   siteMetadata: {
     title: "headphones",
-  },    
-  author: {
-    name: `Craig Milam`,
-    summary: `#100daysofgatsby`,
   },
-  description: `#100daysofgatsby challenge site - audiocore`,
-  siteUrl: `https://preview-startermain.gtsb.io`,
-  social: {
-    twitter: `texasrocker`,
-  },
+  
   plugins: [
     {
       resolve: "gatsby-source-contentful",
       options: {
-        accessToken: "BMQsbl63FDjXsDBv0C0h8cdcwlMpUjS-ep44Lhmj96U",
-        spaceId: "6ttsjfhno0pv",
+        accessToken: "JcqBSD1b8g-o-Rvaovp3hKkuJr82PZX1N6DQYNpwbmM",
+        spaceId: "evegefjcr57r",
       },
     },
     "gatsby-plugin-sharp",
