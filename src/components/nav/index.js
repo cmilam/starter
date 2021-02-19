fed0abff2b36a4d8a42b889408d27cfe16dcae99
cmilam <craig.milam@nearform.com>
2021-02-19 14:57:52 +0100

    nav image further testing

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { graphql, Link } from 'gatsby'
+import { StaticQuery, graphql, Link } from 'gatsby'
 import styles from './style.css'
 import {Flex,
         Box,
@@ -8,37 +8,44 @@ import {Flex,
         Heading} from "@chakra-ui/react"
 import {GatsbyImage} from 'gatsby-plugin-image'
 
-const Nav = ( props ) => (
-    <Box bg="gray.900" paddingX="10%">
-    <Flex maxW="100%">
-        <Box p="2">
-            <GatsbyImage 
-                image={props.data.indexImage.childImageSharp.fluid} 
-            />
-        </Box>
-        <Spacer />
-        <Box>
-            <Button mr="4" marginY="10px">
-                <Link to="/locations">Locations</Link>
-            </Button>
-            <Button>
-                <Link to="/contact">Contact</Link>
-            </Button>
-        </Box>
-    </Flex>
-    </Box>
-  )
 
-export default Nav;
 
-export const query = graphql`
-query {
-  indexImage: file(relativePath: { eq: "nodeconfremote-2021-logo.png" }) {
-    childImageSharp {
-      fluid(quality: 100, maxWidth: 373) {
-        ...GatsbyImageSharpFluid
-      }
-    }
+export default function Nav() {
+    return (
+      <StaticQuery
+        query={graphql`{
+        indexImage: file(relativePath: { eq: "nodeconfremote-2021-logo.png" }) {
+              childImageSharp {
+                fluid(quality: 100, maxWidth: 373) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        `}
+        render={data => (
+            <Box bg="gray.900" paddingX="10%">
+            <Flex maxW="100%">
+                <Box p="2">
+                    <GatsbyImage 
+                        image={data.indexImage.childImageSharp.fluid} 
+                    />
+                </Box>
+                <Spacer />
+                <Box>
+                    <Button mr="4" marginY="10px">
+                        <Link to="/locations">Locations</Link>
+                    </Button>
+                    <Button>
+                        <Link to="/contact">Contact</Link>
+                    </Button>
+                </Box>
+            </Flex>
+            </Box>
+        )}
+      />
+    )
   }
-}
-`;
+  
+
+
